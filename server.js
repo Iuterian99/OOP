2b59c94519121e7b96107f69a023b535ba573651
Iuterian99 <Abduhalim.orziqulov@bk.ru>
2022-01-09 23:41:45 +0500

    did class

---
+++ server.js
@@ -0,0 +1,34 @@
+/*
+Aslida 2 xil programming turi mavjud:
+1) Functional programming (Hook based),
+2) OOP (OBJECT ORIENTED PROGRAMMING) (Class based)
+Har bir "Object" class yordamida tuziladi!
+*/
+
+//! constructor
+// "Constructor" bu oddiy "Attributes" deyiladigan o`zgaruvchi qabul qilish uchun ishlatiladigan class Metodi!
+
+//! this
+// this bu kiritiladiga attributelar yaratilgan class uchun global ekanligini bildiradi!
+// console.log(this); // 1) browserda window , 2) node.js(terminalda) {} global scope chiqaradi
+
+class concatWords {
+  constructor(str1, str2) {
+    this.str1 = str1;
+    this.str2 = str2;
+  }
+
+  getConcat() {
+    return this.str1 + " " + this.str2;
+  }
+
+  getSplit() {
+    return this.str1.split("") + " " + this.str2;
+  }
+}
+
+// console.log(new concatWords("Salom", "Abduhalim").getConcat()); //! class chaqirishni 1-yo`li
+
+//! class chqirishni 2- yo`li
+const joinedWords = new concatWords("abcdefghijklmnopqrstuvwxyz", "alifbo!");
+console.log(joinedWords.getSplit());
