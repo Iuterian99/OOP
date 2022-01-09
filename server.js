cf5b7c1649ab2c0ca2875510c11963a611e4053b
Iuterian99 <Abduhalim.orziqulov@bk.ru>
2022-01-09 23:57:31 +0500

    constructor

--- server.js
+++ server.js
@@ -12,23 +12,44 @@ Har bir "Object" class yordamida tuziladi!
 // this bu kiritiladiga attributelar yaratilgan class uchun global ekanligini bildiradi!
 // console.log(this); // 1) browserda window , 2) node.js(terminalda) {} global scope chiqaradi
 
-class concatWords {
+// class concatWords {
+//   constructor(str1, str2) {
+//     this.str1 = str1;
+//     this.str2 = str2;
+//   }
+
+//   getConcat() {
+//     return this.str1 + " " + this.str2;
+//   }
+
+//   getSplit() {
+//     return (this.str1 + this.str2).split("");
+//   }
+// }
+
+// console.log(new concatWords("Salom", "Abduhalim").getConcat()); //! class chaqirishni 1-yo`li
+
+//! class chqirishni 2- yo`li
+// const joinedWords = new concatWords("abcdefghijkl", "mnopqrstuvwxyz");
+// console.log(joinedWords.getSplit());
+
+//! inheritance
+
+class cola {
   constructor(str1, str2) {
     this.str1 = str1;
     this.str2 = str2;
   }
 
-  getConcat() {
-    return this.str1 + " " + this.str2;
-  }
-
-  getSplit() {
-    return (this.str1 + this.str2).split("");
+  getWaterBoiled() {
+    return "cola`s water is boiled";
   }
 }
 
-// console.log(new concatWords("Salom", "Abduhalim").getConcat()); //! class chaqirishni 1-yo`li
+class fanta extends cola {
+  constructor(str1, str2) {
+    super(str1, str2);
+  }
+}
 
-//! class chqirishni 2- yo`li
-const joinedWords = new concatWords("abcdefghijkl", "mnopqrstuvwxyz");
-console.log(joinedWords.getSplit());
+const fanta = new fanta("ob", "havo");
